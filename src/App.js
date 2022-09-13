9d33ea16e85e010da6befab427b3690632928dc9
brendanburke <bfburke33@gmail.com>
2022-09-13 14:06:12 -0400

    added footer, updated some styling, deploying to netlify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Outlet />
+      <div className="content-wrap">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
     return (
-        <div className="footer-container">Footer Bar</div>
+        <footer>
+            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/brendanfburke/"><FontAwesomeIcon className="footer-icon" icon={faLinkedinIn}  /></a>
+            <a target="_blank" rel="noreferrer" href='https://github.com/brendanfburke'><FontAwesomeIcon className="footer-icon" icon={faGithub}  /></a>
+        </footer>
     )
 }
 
